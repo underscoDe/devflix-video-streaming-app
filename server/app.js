b56ffb541aadd84d22fae6c20a3a4ee67cf6164f
pericles001 <periclesadjovi@outlook.fr>
2022-06-13 20:34:57 +0100

    Edit app script - Create new route for video streaming - Get access to chunks of file

--- server/app.js
+++ server/app.js
@@ -1,16 +1,73 @@
 /*
 *Application main codebase
 */
-const express = require('express')
-const fs = require('fs')
-const path = require('path')
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
+const thumbsupply = require('thumbsupply');
+
+const videos = [
+    {
+        id: 0,
+        poster: '/video/0/poster',
+        duration: '3 mins',
+        name: 'Sample 1'
+    },
+    {
+        id: 1,
+        poster: '/video/1/poster',
+        duration: '4 mins',
+        name: 'Sample 2'
+    },
+    {
+        id: 2,
+        poster: '/video/2/poster',
+        duration: '2 mins',
+        name: 'Sample 3'
+    },
+];
 
 const app = express()
 
-app.get('/video', (req, res) => {
- res.sendFile('assets/sample.mp4' ,{root: __dirname})
+
+app.use(cors)
+app.get('/videos', (req, res) => res.json(videos));
+
+app.get('/video/:id/data', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    res.json(videos[id]);
+});
+
+app.get('/video/:id', (req, res) => {
+    const path = `assets/${req.params.id}.mp4`;
+    const stat = fs.statSync(path);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+    if (range){
+        const parts = range.replace(/bytes=/, "").split("_");
+        const start = parseInt(parts[0], 10);
+        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+        const chunkSize = (end - start) + 1;
+        const file = fs.createReadStream(path, {start, end});
+        const head = {
+            'Content-Range' : `bytes ${start}-${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunkSize,
+            'Content-Type': 'video/mp4',
+        };
+        res.writeHead(206, head);
+        file.pipe(res);
+    }else{
+        const head = {
+            'Content-Length': fileSize,
+            'Content-Type': 'video/mp4',
+        };
+        res.writeHead(200, head);
+        fs.createReadStream(path).pipe(res)
+    }
 })
 
-app.listen(4000, ()=> {
- console.log('Listening on port 4000!')
-})
+app.listen(4000, () => {
+    console.log('Listening on port 4000!')
+});
