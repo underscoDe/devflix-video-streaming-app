6593b0c82510c8efd7e4ec35ae9dccfaa2cbd4d1
abdoachhoubi <abdo.achhoubi2@gmail.com>
2022-07-01 00:44:51 +0100

    Cleaning code

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,36 @@
 import React, { useState, useEffect } from 'react';
 
-import Login from './components/LoginPage';
-import Register from './components/RegisterPage';
-import Home from './components/HomePage';
-import TempPlayer from './components/TempPlayer';
-import firebase  from './services/firebase';
+import { Login, Register, Home, TempPlayer } from './components';
+import firebase from './services/firebase';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
 
-function App() {
+function App () {
 
-  document.documentElement.classList.remove("nav-open");
-  React.useEffect(() => {
-    document.body.classList.add("index");
-    return function cleanup() {
-      document.body.classList.remove("index");
+  document.documentElement.classList.remove( "nav-open" );
+  React.useEffect( () => {
+    document.body.classList.add( "index" );
+    return function cleanup () {
+      document.body.classList.remove( "index" );
     };
-  });
+  } );
 
-  const [user, setUser] = useState(null);
+  const [ user, setUser ] = useState( null );
 
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged(user => {
-      setUser(user);
-    })
-  }, [])
-
-  console.log(user)
+  useEffect( () => {
+    firebase.auth().onAuthStateChanged( user => {
+      setUser( user );
+    } )
+  }, [] );
 
   return (
     <div className="app">
       <Routes>
-        <Route path="/" element={ user ? <Home user={user} /> : <Login /> } />
-        <Route path="login" element={ user ? <Home user={user} /> : <Login /> } />
-        <Route path="register" element={ user ? <Home user={user} /> : <Register /> } />
-        <Route path="/player/:id" element={ user ? <TempPlayer  /> : <Login />}></Route>
+        <Route path="/" element={ user ? <Home user={ user } /> : <Login /> } />
+        <Route path="login" element={ user ? <Home user={ user } /> : <Login /> } />
+        <Route path="register" element={ user ? <Home user={ user } /> : <Register /> } />
+        <Route path="/player/:id" element={ user ? <TempPlayer /> : <Login /> }></Route>
       </Routes>
     </div>
   );
