035282cfba2bb58cb45765f05ad0369280896a22
abdoachhoubi <abdo.achhoubi2@gmail.com>
2022-07-01 00:47:16 +0100

    Added an export file in components directory

---
+++ client/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Login } from "./LoginPage";
+export { default as Register } from "./RegisterPage";
+export { default as Home } from "./HomePage";
+export { default as TempPlayer } from "./TempPlayer";
